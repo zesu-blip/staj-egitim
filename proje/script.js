a8bf61fac862670da8149df38dbbfb4b7dac0aba
zeynep sude <azemisude@gmail.com>
2026-08-03 17:16:38 +0300

    javascript degisken veri tipleri ve operator calismalari eklendi

---
+++ proje/script.js
@@ -0,0 +1,23 @@
+let st = "5";
+let sayi = 5;
+let tamamlandi;
+console.log(typeof "5");
+console.log(typeof 5);
+console.log(typeof tamamlandi);
+console.log(5 + 5);
+console.log("5" + 5);
+console.log("5" - 5);
+console.log("5" * 5);
+console.log("5" / 5);
+console.log("5" % 5);
+console.log(tamamlandi);
+console.log("5" === 5);
+console.log("5" == 5);
+console.log("5" !== 5);
+console.log("5" > 5);
+console.log("5" < 5);
+console.log("5" >= 5);
+console.log("5" <= 5);
+if (5 < 7 && 5 > 3) {
+    console.log("5 3ten büyüktür ve 7den küçüktür");
+}
