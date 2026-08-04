e21b1df815571a66affe90705962eb1001add1e9
zeynep sude <azemisude@gmail.com>
2026-08-04 17:48:50 +0300

    javascript if for while ve fonksiyon çalışmaları eklendi

--- proje/script.js
+++ proje/script.js
@@ -1,4 +1,4 @@
-let st = "5";
+/*let st = "5";
 let sayi = 5;
 let tamamlandi;
 console.log(typeof "5");
@@ -18,6 +18,76 @@ console.log("5" > 5);
 console.log("5" < 5);
 console.log("5" >= 5);
 console.log("5" <= 5);
-if (5 < 7 && 5 > 3) {
-    console.log("5 3ten büyüktür ve 7den küçüktür");
+
+let gorevSayisi = 4;
+if (gorevSayisi == 0) {
+    console.log("liste boş");
 }
+else if (gorevSayisi >= 1 && gorevSayisi <= 5) {
+    console.log("liste kısa");
+}
+else if (gorevSayisi > 5) {
+    console.log("liste dolu");
+}
+
+let gorevSayisi = 4;
+for (let i = 1; i <= 10; i = i + 1) { console.log(i); }
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i = i + 1;
+}
+
+function selamla(ad){
+console.log("merhaba " + ad); }
+selamla("Zeynep");
+
+function topla(a, b){
+    return a + b;
+}
+let sonuc = topla(3, 5);
+console.log(sonuc);
+
+function  topla(a,b){
+    console.log(a + b);
+}
+topla(3, 5);
+
+
+function topla(a,b){
+    console.log(a + b);
+}
+let sonuc = topla(3, 5);
+
+let sonuc2= topla(3, 7);*/
+
+// function topla(a, b) {
+//     return a + b;
+// }
+// let sonuc = topla(3, 5);
+// console.log(sonuc);
+
+// function topla(a, b) {
+// console.log(a + b);
+// }
+// let sonuc = topla(3, 5);
+// let a = 5;
+// let b = 7;
+// function topla(a, b) {
+//     return a + b;
+//     console.log("merhaba");
+// }
+
+function topla(a, b) {
+    return a + b;
+}
+
+let sonuc = topla(3, 5);
+
+console.log(sonuc);
