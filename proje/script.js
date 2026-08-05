4d88e5261a366981a982f65a5f53c477be271b75
zeynep sude <azemisude@gmail.com>
2026-08-05 17:05:34 +0300

    değişken ve fonksiyon örnekleri eklendi

--- proje/script.js
+++ proje/script.js
@@ -1,71 +1,50 @@
-/*let st = "5";
-let sayi = 5;
-let tamamlandi;
-console.log(typeof "5");
-console.log(typeof 5);
-console.log(typeof tamamlandi);
-console.log(5 + 5);
-console.log("5" + 5);
-console.log("5" - 5);
-console.log("5" * 5);
-console.log("5" / 5);
-console.log("5" % 5);
-console.log(tamamlandi);
-console.log("5" === 5);
-console.log("5" == 5);
-console.log("5" !== 5);
-console.log("5" > 5);
-console.log("5" < 5);
-console.log("5" >= 5);
-console.log("5" <= 5);
+// let gorevSayisi = 4;
+// if (gorevSayisi == 0) {
+//     console.log("liste boş");
+// }
+// else if (gorevSayisi >= 1 && gorevSayisi <= 5) {
+//     console.log("liste kısa");
+// }
+// else if (gorevSayisi > 5) {
+//     console.log("liste dolu");
+// }
 
-let gorevSayisi = 4;
-if (gorevSayisi == 0) {
-    console.log("liste boş");
-}
-else if (gorevSayisi >= 1 && gorevSayisi <= 5) {
-    console.log("liste kısa");
-}
-else if (gorevSayisi > 5) {
-    console.log("liste dolu");
-}
+// let gorevSayisi = 4;
+// for (let i = 1; i <= 10; i = i + 1) { console.log(i); }
 
-let gorevSayisi = 4;
-for (let i = 1; i <= 10; i = i + 1) { console.log(i); }
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i = i + 1;
+// }
 
-for (let i = 1; i <= 10; i++) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-}
-let i = 1;
-while (i <= 10) {
-    console.log(i);
-    i = i + 1;
-}
+// function selamla(ad){
+// console.log("merhaba " + ad); }
+// selamla("Zeynep");
 
-function selamla(ad){
-console.log("merhaba " + ad); }
-selamla("Zeynep");
+// function topla(a, b){
+//     return a + b;
+// }
+// let sonuc = topla(3, 5);
+// console.log(sonuc);
 
-function topla(a, b){
-    return a + b;
-}
-let sonuc = topla(3, 5);
-console.log(sonuc);
-
-function  topla(a,b){
-    console.log(a + b);
-}
-topla(3, 5);
+// function  topla(a,b){
+//     console.log(a + b);
+// }
+// topla(3, 5);
 
 
-function topla(a,b){
-    console.log(a + b);
-}
-let sonuc = topla(3, 5);
+// function topla(a,b){
+//     console.log(a + b);
+// }
+// let sonuc = topla(3, 5);
 
-let sonuc2= topla(3, 7);*/
+// let sonuc2= topla(3, 7);
 
 // function topla(a, b) {
 //     return a + b;
@@ -84,10 +63,52 @@ let sonuc2= topla(3, 7);*/
 //     console.log("merhaba");
 // }
 
-function topla(a, b) {
-    return a + b;
+// function topla(a, b) {
+//     return a + b;
+// }
+
+// let sonuc = topla(3, 5);
+
+// console.log(sonuc);
+
+// function problem1() {
+//     for (let i = 1; i <= 20; i++) {
+//         if (i % 3 == 0){
+//             console.log(i + " 3'ün katıdır");
+//         }
+//     }
+// }
+// problem1();
+
+// let a = prompt("Lütfen bir sayı giriniz");
+// function problem2() {
+//     if (a%2==0){
+//         return a + " çifttir";
+//     } 
+//     else {
+//         return a + " tektir";
+//     }
+// }
+// console.log(problem2()); 
+
+// let a = prompt("Lütfen ilk sayıyı giriniz");
+// let b = prompt("Lütfen ikinci sayıyı giriniz");
+// function problem3() {
+//     if (a > b) {
+//         console.log(a + " büyüktür " + b + " sayısından");
+//     }
+//     else if (a == b) {
+//         console.log(a + " eşittir " + b + " sayısına");
+//     }
+//     else {
+//         console.log(b + " büyüktür " + a + " sayısından");
+//     }
+// }
+// problem3(); 
+
+function deneme() {
+    let a = 7;
+    console.log(a);
 }
-
-let sonuc = topla(3, 5);
-
-console.log(sonuc);
+deneme();
+//console.log(a);
