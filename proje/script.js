de79d9eda901d7ff5160371b5dc368121da46ae4
zeynep sude <azemisude@gmail.com>
2026-08-07 18:01:32 +0300

    yorum satirlari temizlendi ve parametreli fonksiyona cevirildi

--- proje/script.js
+++ proje/script.js
@@ -1,114 +1,92 @@
-// let gorevSayisi = 4;
-// if (gorevSayisi == 0) {
-//     console.log("liste boş");
-// }
-// else if (gorevSayisi >= 1 && gorevSayisi <= 5) {
-//     console.log("liste kısa");
-// }
-// else if (gorevSayisi > 5) {
-//     console.log("liste dolu");
-// }
+// 7. liste durumu kontrolü
+let gorevSayisi = 8;
+if (gorevSayisi === 0) {
+    console.log("liste boş");
+}
+else if (gorevSayisi >= 1 && gorevSayisi <= 5) {
+    console.log("liste kısa");
+}
+else if (gorevSayisi > 5) {
+    console.log("liste dolu");
+}
 
-// let gorevSayisi = 4;
-// for (let i = 1; i <= 10; i = i + 1) { console.log(i); }
+// 8. döngü
+for (let i = 1; i <= 10; i = i + 1) { 
+    console.log(i); 
+}
 
-// for (let i = 1; i <= 10; i++) {
-//     if (i % 2 == 0) {
-//         console.log(i);
-//     }
-// }
-// let i = 1;
-// while (i <= 10) {
-//     console.log(i);
-//     i = i + 1;
-// }
+// çift sayıları yazdırma 
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
 
-// function selamla(ad){
-// console.log("merhaba " + ad); }
-// selamla("Zeynep");
+let j = 1; 
+while (j <= 10) {
+    console.log(j);
+    j = j + 1;
+}
 
-// function topla(a, b){
-//     return a + b;
-// }
-// let sonuc = topla(3, 5);
-// console.log(sonuc);
+// 9. fonksiyon
+function selamla(ad) {
+    console.log("merhaba " + ad); 
+}
+selamla("Zeynep");
 
-// function  topla(a,b){
-//     console.log(a + b);
-// }
-// topla(3, 5);
+function topla(a, b) {
+    return a + b;
+}
+let sonuc = topla(3, 5);
+console.log(sonuc);
 
+let sonuc2 = topla(3, 7);
+console.log(sonuc2);
 
-// function topla(a,b){
-//     console.log(a + b);
-// }
-// let sonuc = topla(3, 5);
+// 10. problemler
 
-// let sonuc2= topla(3, 7);
+// problem 1
+function problem1() {
+    for (let i = 1; i <= 20; i++) {
+        if (i % 3 === 0) {
+            console.log(i + " 3'ün katıdır");
+        }
+    }
+}
+problem1();
 
-// function topla(a, b) {
-//     return a + b;
-// }
-// let sonuc = topla(3, 5);
-// console.log(sonuc);
+// problem 2 
+function ciftMi(sayi) {
+    if (sayi % 2 === 0) {
+        return sayi + " çifttir";
+    } 
+    else {
+        return sayi + " tektir";
+    }
+}
+console.log(ciftMi(7)); 
 
-// function topla(a, b) {
-// console.log(a + b);
-// }
-// let sonuc = topla(3, 5);
-// let a = 5;
-// let b = 7;
-// function topla(a, b) {
-//     return a + b;
-//     console.log("merhaba");
-// }
+// problem 3 
+function buyukOlaniBul(a, b) {
+    if (a > b) {
+        return a + " büyüktür " + b + " sayısından";
+    }
+    else if (a === b) {
+        return a + " eşittir " + b + " sayısına";
+    }
+    else {
+        return b + " büyüktür " + a + " sayısından";
+    }
+}
+console.log(buyukOlaniBul(9, 10));
 
-// function topla(a, b) {
-//     return a + b;
-// }
-
-// let sonuc = topla(3, 5);
-
-// console.log(sonuc);
-
-// function problem1() {
-//     for (let i = 1; i <= 20; i++) {
-//         if (i % 3 == 0){
-//             console.log(i + " 3'ün katıdır");
-//         }
-//     }
-// }
-// problem1();
-
-// let a = prompt("Lütfen bir sayı giriniz");
-// function problem2() {
-//     if (a%2==0){
-//         return a + " çifttir";
-//     } 
-//     else {
-//         return a + " tektir";
-//     }
-// }
-// console.log(problem2()); 
-
-// let a = prompt("Lütfen ilk sayıyı giriniz");
-// let b = prompt("Lütfen ikinci sayıyı giriniz");
-// function problem3() {
-//     if (a > b) {
-//         console.log(a + " büyüktür " + b + " sayısından");
-//     }
-//     else if (a == b) {
-//         console.log(a + " eşittir " + b + " sayısına");
-//     }
-//     else {
-//         console.log(b + " büyüktür " + a + " sayısından");
-//     }
-// }
-// problem3(); 
-
+// scope
 function deneme() {
-    let a = 7;
-    console.log(a);
+    let x = 7; 
+    console.log(x);
 }
 deneme();
-//console.log(a);
+
+// boolean
+let durum = true;
+console.log(typeof durum);
