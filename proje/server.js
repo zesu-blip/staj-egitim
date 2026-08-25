67a8fc46128b738cab13b518ec18fcf4d7cd6702
zeynep sude <azemisude@gmail.com>
2026-08-25 14:59:33 +0300

    gorevlere id eklendi, silme id ile yapiliyor

--- proje/server.js
+++ proje/server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 
@@ -9,22 +10,55 @@ app.get("/selam", function (req, res) {
 });
 
 let gorevler = [
-    { metin: "ekmek al", bitti: false },
-    { metin: "kitabı bitir", bitti: true },
-    { metin: "markete git", bitti: false }
+    { id: 1, metin: "ekmek al", bitti: false },
+    { id: 2, metin: "kitabı bitir", bitti: true },
+    { id: 3, metin: "markete git", bitti: false }
 ];
+
+function kaydet() {
+    fs.writeFileSync("gorevler.json", JSON.stringify(gorevler, null, 2));
+}
+
+if (fs.existsSync("gorevler.json")) {
+    gorevler = JSON.parse(fs.readFileSync("gorevler.json", "utf8"));
+} else {
+    kaydet();
+}
 
 app.get("/gorevler", function (req, res) {
     res.json(gorevler);
 });
 
 app.post("/gorevler", function (req, res) {
-    gorevler.push(req.body);
+    if (req.body.metin === undefined || req.body.metin === "") {
+        res.status(400).send("metin bos olamaz");
+        return;
+    }
+    gorevler.push({
+        id: Date.now(),
+        metin: req.body.metin,
+        bitti: false
+    });
+    kaydet();
     res.json(gorevler);
 });
 
-app.delete("/gorevler/:index", function (req, res) {
-    gorevler.splice(Number(req.params.index), 1);
-    res.json(gorevler); })
+app.delete("/gorevler/:id", function (req, res) {
+    let index = gorevler.findIndex(function (g) {
+        return g.id === Number(req.params.id);
+    });
+    gorevler.splice(index, 1);
+    kaydet();
+    res.json(gorevler);
+});
+
+app.put("/gorevler/:id", function (req, res) {
+    let gorev = gorevler.find(function (g) {
+        return g.id === Number(req.params.id);
+    });
+    gorev.bitti = !gorev.bitti;
+    kaydet();
+    res.json(gorevler);
+});
 
 app.listen(3000);
